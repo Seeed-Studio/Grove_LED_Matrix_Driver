5393c37ac553b1f4b934ff76ef332e265702ab54
victor-hecs <herseed@163.com>
2016-05-05 11:45:22 +0800

    Update test_demo.ino

--- Arduino/LEDMatrix/example/test_demo/test_demo.ino
+++ Arduino/LEDMatrix/example/test_demo/test_demo.ino
@@ -1,11 +1,11 @@
 /*
  * test_demo.ino
- * A quick start example for test all functions of the Grove-Led Matrix. 
- * Description: This demo let you can send instruction 0-8 to test all functions of the Grove-Led Matrix, via the serial port.
- *              And you also can modify the function arguments to test.
+ * A quick start example for test all functions of the Grove - LED Matrix driver. 
+ * Description: This demo let you send code 0-8 to test all functions of the Grove-Led Matrix, via the serial port.
+ *              And you also can alter the function parameters to adapt actual situations.
  *
  * Copyright (c) 2016 seeed technology inc.
- * Website    : www.seeed.cc
+ * Website    : www.seeedstudio.com
  * Author     : Wuruibin
  * Created Time: Jan 2016
  * Modified Time:
@@ -35,9 +35,9 @@
 #include <SoftwareSerial.h>
 #include "LEDMatrix.h"
 
-// Note: You must define a SoftwareSerial class object that the name must be "matrix_serial", 
-//       but you can change the pin number according to the actual situation.
-#define RECEIVE_PIN      2
+// Note: You must define a SoftwareSerial class object and the ojbect's name must be "matrix_serial". 
+//       You can change the pin number according to the actual situation.
+#define RECEIVE_PIN      2 //Plugin to D2 socket on Base Shield.
 #define TRANSMIT_PIN     3
 SoftwareSerial matrix_serial(RECEIVE_PIN, TRANSMIT_PIN);
 
@@ -96,7 +96,7 @@ void loop()
             {
                 for (i = 0; i <= 63; i++)
                 {
-                    led.drawPoint(i, j, GREEN);     // draw LED Point
+                    led.drawPoint(i, j, GREEN);     // Turn on a LED in LED Matrix
                     delay(10);
                 }
             }
@@ -111,7 +111,7 @@ void loop()
             break;
             
         case '3':
-            led.drawLine(5, 3, 62, 3, RED);         // draw Line
+            led.drawLine(5, 3, 62, 3, RED);         // draw a Line
             led.drawLine(5, 5, 62, 5, RED);
             led.drawLine(61, 1, 61, 30, GREEN);
             led.drawLine(62, 1, 62, 30, GREEN);
@@ -130,7 +130,7 @@ void loop()
         case '5':
             for (i = 0; i <= 127; i++)
             {
-                led.displayChar(5, 0, i, FONT_6X8, RED);        // display Char. ASCII Value Range(Dec):0-127
+                led.displayChar(5, 0, i, FONT_6X8, RED);        // display Char. ASCII Value Range:0-127(Decimal)
                 delay(2000);
             }
             Serial.println( "5" );
@@ -140,7 +140,7 @@ void loop()
             break;
             
         case '6':
-            led.drawExpression(0, 0, SMILE_32X20, GREEN);       // draw Expression. A total of 7 kinds of expressions. Pay attention to the expression image size    
+            led.drawExpression(0, 0, SMILE_32X20, GREEN);       // draw emoji. A total of 7 kinds of expressions. Pay attention to the different emoji size    
             delay(2000);
             led.clear();
             led.drawExpression(0, 0, SAD_32X32, GREEN);
@@ -165,12 +165,12 @@ void loop()
             break;
             
         case '7':
-            led.drawImage(16, 2, 32, 20, seeedLogo_32x20, GREEN);       // draw image. This example provides an example image data. You can use your own image data. Note: Max:64*64!
+            led.drawImage(16, 2, 32, 20, seeedLogo_32x20, GREEN);       // raw image. This example provides an example image data. You can use your own image data. Note matrix dimensions: Max:32*64!
             Serial.println( "7" );
             break;
 
         case '8':
-            led.displayString(0, 0, cString, 7, FONT_6X8, GREEN);       // display String
+            led.displayString(0, 0, cString, 7, FONT_6X8, GREEN);       // display an string
             led.displayString(0, 10, "a!@#$%^&*", 9, FONT_10X16, GREEN);
             Serial.println( "8" );
             break;
